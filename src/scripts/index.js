cc071c462c74d86841a21294a039df9ad9a37079
KishoreSrinivasa <itsmekishore2003@gmail.com>
2024-07-20 06:49:43 +0530

    code updation

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,25 +1,41 @@
+function setLogoandName(logourl, name) {
+  document.getElementById("company-logo").src = logourl;
+
+  document.getElementById("company-name").innerHTML = name;
+  document.getElementById('company-name-02').innerHTML = name;
+} 
+
 document.addEventListener('DOMContentLoaded', () => {
     "use strict"
 
-//for swiper funtion
-const swiper = new Swiper(".sliderFeaturedPosts", {
-    spaceBetween: 0,
-    speed: 500,
-    centeredSlides: true,
-    loop: true,
-    slideToClickedSlide: true,
-    autoplay: {
-      delay: 3000,
-      disableOnInteraction: false,
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    navigation: {
-      nextEl: ".custom-swiper-button-next",
-      prevEl: ".custom-swiper-button-prev",
-    },
+    //creating some constant variable 
+    const logo_url = "assets/components/down-left-image-removebg-preview.png";
+    const company_name = "Kaikuttai";
+
+    setLogoandName(logo_url, company_name);
+
+
+
+  //for swiper funtion
+  const swiper = new Swiper(".sliderFeaturedPosts", {
+      spaceBetween: 0,
+      speed: 500,
+      centeredSlides: true,
+      loop: true,
+      slideToClickedSlide: true,
+      autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      navigation: {
+        nextEl: ".custom-swiper-button-next",
+        prevEl: ".custom-swiper-button-prev",
+      },
+    });
   });
-});
+
 
