c155a3ed990dab6ea51a5f87466b6157299941c1
KishoreSrinivasa <itsmekishore2003@gmail.com>
2024-07-25 17:34:10 +0530

    Code update

--- src/scripts/cart.js
+++ src/scripts/cart.js
@@ -13,4 +13,26 @@ function setLogoandName(logourl, name) {
       const company_name = "Kaikuttai";
   
       setLogoandName(logo_url, company_name);
+
+
+      //for swiper funtion
+    const swiper = new Swiper(".sliderFeaturedPosts", {
+        spaceBetween: 0,
+        speed: 500,
+        centeredSlides: true,
+        loop: true,
+        slideToClickedSlide: true,
+        autoplay: {
+            delay: 3000,
+            disableOnInteraction: false,
+        },
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        navigation: {
+            nextEl: ".custom-swiper-button-next",
+            prevEl: ".custom-swiper-button-prev",
+        },
+    });
   });
