823179c6d4f0c1cbd0b8a7e8859d4de4dd971c3f
Kishore. S <109349956+KishoreSrinivasa@users.noreply.github.com>
2024-08-24 12:58:52 +0530

    Update firebaseSDK.js

--- src/scripts/firebaseSDK.js
+++ src/scripts/firebaseSDK.js
@@ -6,13 +6,13 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.4/fireba
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyArCf4oFbwO-TtG9pE7pz7IwZy4PNEqG4s",
-  authDomain: "ecommerce-webapp-65e26.firebaseapp.com",
-  projectId: "ecommerce-webapp-65e26",
-  storageBucket: "ecommerce-webapp-65e26.appspot.com",
-  messagingSenderId: "259506750132",
-  appId: "1:259506750132:web:a9729f69b2061061a54de1",
-  measurementId: "G-4DN2ZE317S"
+  apiKey: "--",
+  authDomain: "--",
+  projectId: "--",
+  storageBucket: "--",
+  messagingSenderId: "--",
+  appId: "--",
+  measurementId: "--"
 };
 
 // Initialize Firebase
